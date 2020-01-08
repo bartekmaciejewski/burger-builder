ca403d4322f046427b0f737519f0eae44475ba00
bmaciejewski <bmaciejewski@pc31.home>
2020-01-09 00:36:45 +0100

    rendering ingredients from preset object

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ## Burger builder app
 
 The project created during https://www.udemy.com/course/react-the-complete-guide-incl-redux/.
-
-
-##### CSS module after eject
-https://github.com/facebook/create-react-app/issues/5453
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import Ingredient from "./Ingredients/Ingredient";
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [
+            ...Array(props.ingredients[igKey])
+        ].map((_, i) => {
+            return <Ingredient key={igKey + i} type={igKey} />
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <Ingredient type="bread-top" />
-            <Ingredient type="cheese" />
-            <Ingredient type="meat" />
+            { transformedIngredients }
             <Ingredient type="bread-bottom" />
         </div>
     );
