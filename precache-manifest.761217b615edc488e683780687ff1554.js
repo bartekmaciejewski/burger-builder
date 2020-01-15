d13e298b8f7466ad3cd9af7582a5d418c7201536
bmaciejewski <bmaciejewski@pc31.home>
2020-01-15 23:17:20 +0100

    Updates

---
+++ precache-manifest.761217b615edc488e683780687ff1554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89b4328f603b9f1c7c83d160cfa58c4",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "92a9468c7a63519dff2c",
+    "url": "/burger-builder/static/css/main.ec958ac9.chunk.css"
+  },
+  {
+    "revision": "7a052d46bea6b558b421",
+    "url": "/burger-builder/static/js/2.26e84117.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burger-builder/static/js/2.26e84117.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92a9468c7a63519dff2c",
+    "url": "/burger-builder/static/js/main.9cfb6bdb.chunk.js"
+  },
+  {
+    "revision": "daf1555dbb2fc36982e6",
+    "url": "/burger-builder/static/js/runtime-main.e24fe18f.js"
+  }
+]);
