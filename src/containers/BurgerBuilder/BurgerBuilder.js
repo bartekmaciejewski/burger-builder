db1a07cef2911111f9f89a8fcd0316ae4128f835
bmaciejewski <mbartekm@gmail.com>
2020-01-21 17:21:48 +0100

    burger controllers ready, price displayed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 2.5,
+    bacon: 4,
+    meat: 10,
+    cheese: 0.5
+};
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,14 +17,51 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 12
+    };
+
+    addIngredientHandler = (type) => {
+        const updatedCount = this.state.ingredients[type] + 1;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
+    };
+
+    removeIngredientHandler = (type) => {
+        const updatedCount = this.state.ingredients[type] - 1;
+        if (updatedCount < 0) return;
+
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
     };
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let k in disabledInfo) {
+            disabledInfo[k] = disabledInfo[k] <= 0;
+        }
+
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                />
             </Aux>
         );
     }
