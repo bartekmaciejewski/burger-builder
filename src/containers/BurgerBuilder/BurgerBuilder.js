cde1da76ddadaba7fcf955bf4bbcdbaac46ca716
maciejewski.b <mbartekm@gmail.com>
2020-01-24 23:01:52 +0100

    show modal on click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,12 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 12,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
+    };
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
     };
 
     updatePurchaseState(ingredients) {
@@ -72,7 +77,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -82,6 +87,7 @@ class BurgerBuilder extends Component {
                     price={this.state.totalPrice}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
+                    ordered={this.purchaseHandler}
                 />
             </Aux>
         );
